4bfd67810989e92bf26c0b90c97d60be3c926baa
sagarshah4819 <ss3958@njit.edu>
2018-12-15 10:25:30 -0800

    Task: Created a RepliedToQuestion.php page in order to get notification when someone replies to question.

---
+++ app/Notifications/RepliedToQuestion.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Http\Request;
+class RepliedToQuestion extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $request = Request::capture();
+        //dd($request);
+        $question_path = $request->path();
+        $question_url = explode("/", $question_path);
+
+        return (new MailMessage)
+                    ->line('You have a new answer to your question.')
+                    ->action('View answer', \route('question.show', $question_url[1]))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
