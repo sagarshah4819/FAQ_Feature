6d97de4805d5fbf999591020afbc520d3235588f
sagarshah4819 <ss3958@njit.edu>
2018-12-15 10:36:50 -0800

    Task: Created UpdatedReplyToQuestion.php page in order to get notification when someone updates the answer to your question.

---
+++ app/Notifications/UpdatedReplyToQuestion.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Http\Request;
+use App\Question;
+use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Answer;
+use App\User;
+
+class UpdatedReplyToQuestion extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $request = Request::capture();
+        //dd($request);
+        $question_path = $request->path();
+        $question_url = explode("/", $question_path);
+
+        return (new MailMessage)
+                    ->line('Answer is updated to your question.')
+                    ->action('View updated answer', \route('question.show', $question_url[1]))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
