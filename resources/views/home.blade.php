81ba7891cba13193942a97b76ea5a517f799da92
sagarshah4819 <ss3958@njit.edu>
2018-12-15 13:47:09 -0800

    Fix: Fixed home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,7 +29,7 @@
                                                     <a class="btn btn-primary float-right" href="{{ route('question.show', ['id' => $question->id]) }}">
                                                         View
                                                     </a>
-                                                    <p class="card-text float-left">{{$question->user_id}}
+                                                    <p class="card-text float-left"> Posted by user id {{$question->user_id}}
                                                 </p>
                                             </div>
                                         </div>
